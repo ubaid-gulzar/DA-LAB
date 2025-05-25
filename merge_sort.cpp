1499e2948dcf8bdd3a5f8c8f725913886b276596
Ubaid Gulzar Mir <mirubaid610@gmail.com>
2025-05-25 20:29:02 +0530

    merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,59 +1,54 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-void merge(int arr[], int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    int L[n1], R[n2];
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
+void merge( int arr[],int si, int ei, int mid){
+     vector<int> temp;
+     int i=si;
+     int j=mid+1;
+     while(i<=mid && j<=ei){
+        if(arr[i]<= arr[j]){
+            temp.push_back(arr[i++]);
         }
-        k++;
-    }
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void mergeSort(int arr[], int left, int right) {
-        if(left>=right){
-            return;
+        else{
+            temp.push_back(arr[j++]);
         }
-
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+     }
+     while (i<=mid)
+     {
+        temp.push_back(arr[i++]);
+     }
+     while (j<=ei)
+     {
+        temp.push_back(arr[j++]);
+     }
+     // coppy vactor elements to our original array
+     for(int idx=si,x=0;idx<=ei;idx++){
+        arr[idx]=temp[x++];
+     }
 }
 
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+void merge_sort(int arr[],int si, int ei){
+    if(si>=ei){
+        return;
+    }
+     int mid=si+(ei-si)/2;    // to calculate mid element
+     merge_sort(arr, si,mid);   //left half
+     merge_sort(arr, mid+1,ei);  //right half
+
+     merge(arr, si, ei, mid);   //conquer
 }
 
-int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    printArray(arr, size);
-    mergeSort(arr, 0, size - 1);
-    printArray(arr, size);
-     return 0;
+void printarr(int arr[],int n){
+   for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+   }
+}
+
+int main(){
+    int arr[6]={6,3,7,5,2,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    merge_sort(arr , 0 , n-1);
+    printarr(arr , n);
+    return 0;
 }
